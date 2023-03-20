332a0746ffdfee18c4b3056e3b8bb8f90a6736c9
lpnizmeee <pnam020103@gmail.com>
2023-03-21 01:52:54 +0700

    Add template engine

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 const express = require('express')
+const path = require('path')
 const morgan = require('morgan')
-import handlebars from './express-handlebars';
+const { engine }  = require('express-handlebars');
+
 const app = express()
 const port = 3000
 //HTTP logging
 app.use(morgan('combined'))
 
 //Template engine
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.engine('hbs', engine({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
 })
 
+app.get('/news', (req, res) => {
+  res.render('news');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
